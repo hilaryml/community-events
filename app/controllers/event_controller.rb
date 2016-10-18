ea3dec88caea88be047c59db170222efdda2ee99
hilaryml <hilyttle@gmail.com>
2016-10-18 13:57:20 -0400

    Modify event controller

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -4,4 +4,19 @@ class EventController < SessionController
     erb :"events/new"
   end
 
+  post "/new" do
+    @event = Event.new(title: params[:title], date: params[:date], time: params[:time], location: params[:location], description: params[:description], contact: params[:contact])
+    if @event.save
+      @event.user = current_user
+      redirect "/events/#{@event.id}"
+    else
+      redirect "/events/new"
+    end
+  end
+
+  get "/events/:id" do
+    @event = Event.find(params[:id])
+    erb :"events/show"
+  end
+
 end
