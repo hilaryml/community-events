b93a2412b35308e6b2fe97a82b3fc63b59bc764f
hilaryml <hilyttle@gmail.com>
2016-10-18 13:20:42 -0400

    Modify signup view and user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,24 @@
 class UserController < SessionController
 
-  def "/signup" do
+  get "/signup" do
     erb :"/users/signup"
   end
 
-  def "/login" do
+  post "/signup" do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      login(params[:username], params[:password])
+    else
+      redirect "/signup"
+    end
+  end
+
+  get "/login" do
     erb :"users/login"
+  end
+
+  post "/login" do
+    
   end
 
 end
